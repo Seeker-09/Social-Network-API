30742fc26da484c88893957433c3ef7daffb0863
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-08-18 15:19:17 -0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose")
+
+// Schemas
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // validate email. email regex 
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill in a valid email']
+        }, 
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    }
+)
+
+// Virtuals
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+// Create model
+const User = model("User", UserSchema)
+
+// export
+module.exports = User
